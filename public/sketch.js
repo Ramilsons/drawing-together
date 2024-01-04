d758b4947cd816d303b3b30090147273a0f6cba8
RamlilsonSilva <ramilson.silva@rocky.ag>
2024-01-04 14:30:38 -0300

    feat(socketio): add socket io

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,32 @@
+let socket;
+
 function setup() {
   createCanvas(350, 350);
+  background('#1b1b1b');
+
+  socket = io.connect('http://localhost:3000/');
+  socket.on('mouseOpponent', newDrawing);
+}
+
+function newDrawing(data) {
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 15, 15);
+}
+
+function mouseDragged() {
+  let dataMouse = {
+    x: mouseX,
+    y: mouseY
+  }
+
+  socket.emit('mouse', dataMouse);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 15, 15);
 }
 
 function draw() {
-  background('#1b1b1b');
-  ellipse(mouseX, mouseY, 15, 15)
+
 }
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 let express = require('express');
-
+let socket = require('socket.io');
 
 let app = express();
 
 app.use(express.static('public'));
 
 let server = app.listen('3000');
+
+let io = socket(server);
+
+// listener triggered ever connection
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('New Connection', socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouseOpponent', data)
+    }
+}
+
